316d1b70c924e9fea0b49890357acd0f00783350
Flavio-Pestana <flaviopestan@gmail.com>
2021-10-23 14:03:34 -0300

    adicionado home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route } from 'react-router-dom';
 import Signup from './components/Signup';
+import Home from './components/Home'
 
 
 function App() {
   return (
     <div className="App">
       <Route exact path='/signup' component={Signup} />
+      <Route exact path='/' component={Home} />
 
     </div >
   );
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react';
+
+const Home = () => {
+  return (
+    <h1>Home</h1>
+  )
+}
+
+
+export default Home;
