87c863548b668b7b00c3911ded19713950abadc6
Flavio-Pestana <flaviopestan@gmail.com>
2021-10-23 17:01:15 -0300

    adicionado card clientes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <>
+      <div className='p-2 bg-secondary d-flex'>
+        <div className='card w-25'>
+          <img className='img-thumbnail' src='' alt=''/>
+          <h3 className='card-header'>Clientes</h3>
+          <div className='card-body'>
+            <p>Nossos Clientes</p>
+          </div>
+        </div>                
+      </div>
+
+    </>
   )
 }
 
